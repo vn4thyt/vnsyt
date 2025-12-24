2cb5fa46a86c92c1088511b8969a9cf737449203
vn4thyt <vns.on.ytbe@gmail.com>
2025-12-24 13:56:45 +0000

    Update IPInfo.py

--- Stuff/ExozMT/IPInfo/IPInfo.py
+++ Stuff/ExozMT/IPInfo/IPInfo.py
@@ -1,3 +1,7 @@
+# Disclaimer: This is only for entertainment and educational purposes.  
+# I’m not responsible for what you do with it or any consequences.  
+# Made by VN :3
+
 import asyncio
 import aiohttp
 import ipaddress
