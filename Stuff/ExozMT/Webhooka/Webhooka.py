5ebfab44342712b3f3028d8b249f634b3a7fab65
vn4thyt <vns.on.ytbe@gmail.com>
2025-12-24 13:56:55 +0000

    Update Webhooka.py

--- Stuff/ExozMT/Webhooka/Webhooka.py
+++ Stuff/ExozMT/Webhooka/Webhooka.py
@@ -1,3 +1,7 @@
+# Disclaimer: This is only for entertainment and educational purposes.  
+# I’m not responsible for what you do with it or any consequences.  
+# Made by VN :3
+
 import requests
 import base64
 import aiohttp
