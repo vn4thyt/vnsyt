1703de5703a4cedc768da5ae9af46e4d440ed696
vn4thyt <vns.on.ytbe@gmail.com>
2025-12-24 15:42:42 +0000

    Update template.py

--- Stuff/RAT/template.py
+++ Stuff/RAT/template.py
@@ -1,5 +1,5 @@
 # Disclaimer: This is only for entertainment and educational purposes.  
-# I’m not responsible for what you do with it or any consequences.  
+# I am not responsible for what you do with it or any consequences.  
 # Made by VN :3
 
 # If you got ratted, and reversed engineered the file back here - Please note I take NO accountability for what others do with my projects. If you would like to know how to remove the rat and stay safe, please DM 'vn4th' on Discord.
