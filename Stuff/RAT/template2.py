710f9be786795a785f97c4fc887ab43e365e6ebb
vn4thyt <vns.on.ytbe@gmail.com>
2025-12-24 16:22:03 +0000

    Create template2.py

---
+++ Stuff/RAT/template2.py
@@ -0,0 +1,14 @@
+# Disclaimer: This is only for entertainment and educational purposes.  
+# I am not responsible for what you do with it or any consequences.  
+# Made by VN :3
+
+# If you got ratted, and reversed engineered the file back here - Please note I take NO accountability for what others do with my projects. If you would like to know how to remove the rat and stay safe, please DM 'vn4th' on Discord.
+
+TOKEN = f"{token}"
+WHITELIST = [{whitelist}]
+main_channel = {main_channel}
+prefix = f"{prefix}"
+add_to_startup = {add_to_startup}
+silent_mode = {silent_mode}
+
+print(TOKEN, WHITELIST, main_channel, prefix, add_to_startup, silent_mode)
