f0a2927b4320be694e839c30f519b7391ebdf649
vn4thyt <vns.on.ytbe@gmail.com>
2026-04-10 23:49:00 +0100

    Update template.py

--- Stuff/Discord Token Grabber/ignore/template.py
+++ Stuff/Discord Token Grabber/ignore/template.py
@@ -1,3 +1,7 @@
+# Disclaimer: This is only for entertainment and educational purposes.  
+# I’m not responsible for what you do with it or any consequences.  
+# Made by Vexi :3
+
 import os
 import re
 import requests
