ee472b89ca4dd40236194067b449eef72d09cfc7
vn4thyt <vns.on.ytbe@gmail.com>
2026-04-11 03:00:21 +0100

    Update build.py

--- Stuff/Discord Token Grabber/build.py
+++ Stuff/Discord Token Grabber/build.py
@@ -1,5 +1,5 @@
 # Disclaimer: This is only for entertainment and educational purposes.  
-# I’m not responsible for what you do with it or any consequences.  
+# I'm not responsible for what you do with it or any consequences.  
 # Made by Vexi :3
 
 import os
