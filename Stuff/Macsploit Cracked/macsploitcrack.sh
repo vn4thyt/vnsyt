7784d957767eaed174aef7b613b50743a176fb77
vn4thyt <vns.on.ytbe@gmail.com>
2026-06-07 22:36:46 +0100

    Update macsploitcrack.sh

--- Stuff/Macsploit Cracked/macsploitcrack.sh
+++ Stuff/Macsploit Cracked/macsploitcrack.sh
@@ -4,17 +4,21 @@
 
 #!/bin/zsh
 clear
-echo "Cracking MacSploit"
 echo "Thanks to Vexi, this product is brought to you for free! 🎀"
+echo "Cracking MacSploit"
 
 killall RobloxPlayer 2>/dev/null
 killall RobloxPlayerBeta 2>/dev/null
 
-echo "Please wait, macsploit is being installed and cracked."
+echo "Please wait..."
 
-ver=$(curl -s "https://git.raptor.fun/main/version.json" | grep -o '"clientVersionUpload":"[^"]*"' | cut -d '"' -f4)
+if command -v jq &> /dev/null; then
+    ver=$(curl -s "https://git.raptor.fun/main/version.json" | jq -r ".clientVersionUpload")
+else
+    ver=$(curl -s "https://git.raptor.fun/main/version.json" | grep -o '"clientVersionUpload":"[^"]*"' | cut -d '"' -f4)
+fi
+
 curl -O "http://setup.rbxcdn.com/mac/arm64/$ver-RobloxPlayer.zip"
-
 unzip -o -q "$ver-RobloxPlayer.zip"
 sudo mv ./RobloxPlayer.app /Applications/Roblox.app
 rm "$ver-RobloxPlayer.zip"
@@ -25,16 +29,15 @@ sudo mv ./MacSploit.app /Applications/MacSploit.app
 rm macsploit.zip
 
 curl -s -O "https://git.raptor.fun/arm/macsploit.dylib"
+
 if [[ ! -f "./insert_dylib" ]]; then
     curl -s -O "https://raw.githubusercontent.com/Tyilo/insert_dylib/master/insert_dylib"
     chmod +x insert_dylib
 fi
 
 sudo mv ./macsploit.dylib /Applications/Roblox.app/Contents/MacOS/macsploit.dylib
-
 sudo codesign --remove-signature /Applications/Roblox.app
 sudo ./insert_dylib /Applications/Roblox.app/Contents/MacOS/macsploit.dylib /Applications/Roblox.app/Contents/MacOS/RobloxPlayer --strip-codesign --all-yes
-
 sudo mv /Applications/Roblox.app/Contents/MacOS/RobloxPlayer_patched /Applications/Roblox.app/Contents/MacOS/RobloxPlayer
 
-echo "Installed Macsploit cracked! Enjoy :3"
+echo "Installed macsploit crack! Enjoy :3"
